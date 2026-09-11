c0dfbb93d4f03c71e4a16d50f16889eb0ed9b382
lachlanjaimc <lachlanjaimc@gmail.com>
2026-09-11 11:21:50 -0600

    Fixed incorrect resource name for PVE API token

--- global/pve/main.tf
+++ global/pve/main.tf
@@ -19,7 +19,7 @@ provider "proxmox" {
   insecure = true
 }
 
-resource "proxmox_api_token" "api_token" {
+resource "proxmox_user_token" "api_token" {
   comment         = "Managed by Terraform"
   expiration_date = "2033-01-01T22:00:00Z"
   token_name      = "gh_tf"
